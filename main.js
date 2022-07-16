f0dd86b442551a8b4b6aa227796bedae9226e64a
Bonjardim <wellingtonbonjardim7@gmail.com>
2022-07-15 23:21:24 -0300

    dando ação de click ao botão

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function toggle() {
     let btn = document.getElementById('btn')
+    let principal = document.getElementById('principal')
     let vantagens = document.getElementById('vantagens')
 
     if(vantagens.classList.contains('show')) {
         vantagens.classList.remove('show')
     } else {
         vantagens.classList.add('show')
+        principal.remove()
     }
     btn.addEventListener('click', toggle)
 }
-
